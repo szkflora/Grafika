45c104cad9b180e4436df60f500962c88cfb5221
Flora Szallos-Kis <szkisflora@gmail.com>
2025-03-15 21:46:30 +0200

    Okés verzió

--- Program.cs
+++ Program.cs
@@ -144,7 +144,7 @@ namespace Szeminarium1
             };
 
             uint vertices = Gl.GenBuffer();
-            //Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
+            Gl.BindBuffer(GLEnum.ArrayBuffer, vertices);
             Gl.BufferData(GLEnum.ArrayBuffer, (ReadOnlySpan<float>)vertexArray.AsSpan(), GLEnum.StaticDraw);
             Gl.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 0, null);
             Gl.EnableVertexAttribArray(0);
